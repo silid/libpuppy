cd308696dcab62d05faaf8462ffef6dbcfe8530b
Erkki Seppala <ese@mese.(none)>
2008-09-08 22:27:50 +0300

    puppytest: reindented

--- puppytest.c
+++ puppytest.c
@@ -3,22 +3,27 @@
 
 #include "libpuppy.h"
 
-int
-main(int a_c, char** a_v)
+int main(int a_c, char **a_v)
 {
-  puppy_t p = puppy_open(NULL);
-  if (puppy_ok(p)) {
-    const char* dir = a_c > 1 ? a_v[1] : "";
-    puppy_dir_entry_t* entries = puppy_hdd_dir(p, dir);
-    puppy_dir_entry_t* ptr = entries;
-    while (ptr->name[0]) {
-      printf("%d %s\n", ptr->time, ptr->name);
-      ++ptr;
+    puppy_t p = puppy_open(NULL);
+
+    if(puppy_ok(p))
+    {
+        const char *dir = a_c > 1 ? a_v[1] : "";
+        puppy_dir_entry_t *entries = puppy_hdd_dir(p, dir);
+        puppy_dir_entry_t *ptr = entries;
+
+        while(ptr->name[0])
+        {
+            printf("%d %s\n", ptr->time, ptr->name);
+            ++ptr;
+        }
+        free(entries);
     }
-    free(entries);
-  } else {
-    fprintf(stderr, "Failed to initialize puppy\n");
-  }
-  puppy_done(p);
-  return 0;
+    else
+    {
+        fprintf(stderr, "Failed to initialize puppy\n");
+    }
+    puppy_done(p);
+    return 0;
 }
