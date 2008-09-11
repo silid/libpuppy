139bdd718b2c9465edb710be287dc983406cd94a
Erkki Seppala <ese@mese.(none)>
2008-09-11 19:54:21 +0300

    Convert slashes to backslashes, support globs

--- puppytest.c
+++ puppytest.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <fnmatch.h>
 
 #include "libpuppy.h"
+
+char *convert_slashes(const char *a_str)
+{
+    int escape = 0;
+    char *buffer = NULL;
+    char *write = NULL;
+
+    for(int stage = 0; stage < 2; ++stage)
+    {
+        int len = 0;
+
+        for(int c = 0; a_str[c]; ++c)
+        {
+            char ch = a_str[c];
+
+            if(escape || ch != '\\')
+            {
+                if(stage == 1)
+                {
+                    buffer[len] = (ch == '/' ? '\\' : ch);
+                }
+                ++len;
+                escape = 0;
+            }
+            else
+            {
+                escape = 1;
+            }
+        }
+        if(stage == 0)
+        {
+            buffer = write = malloc(len + 1);
+        }
+        else
+        {
+            buffer[len] = 0;
+        }
+    }
+    return buffer;
+}
 
 int main(int a_c, char **a_v)
 {
@@ -9,16 +51,26 @@ int main(int a_c, char **a_v)
 
     if(puppy_ok(p))
     {
-        const char *dir = a_c > 1 ? a_v[1] : "";
+        char *dir = convert_slashes(a_c > 1 ? a_v[1] : "");
+        const char *glob = a_c > 2 ? a_v[2] : "*";
+
         puppy_dir_entry_t *entries = puppy_hdd_dir(p, dir);
         puppy_dir_entry_t *ptr = entries;
 
         while(ptr->name[0])
         {
-            printf("%d %s\n", ptr->time, ptr->name);
+            if(fnmatch(glob, ptr->name, 0) == 0)
+            {
+                char tmStr[1024];
+                struct tm tm = *localtime(&ptr->time);
+
+                strftime(tmStr, sizeof(tmStr), "%Y-%m-%d %H:%M:%S", &tm);
+                printf("%s %s\n", tmStr, ptr->name);
+            }
             ++ptr;
         }
         free(entries);
+        free(dir);
     }
     else
     {
