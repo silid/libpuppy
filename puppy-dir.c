9d385fbef0b3799f8f7a6dfc5ed8a67428216f8c
Erkki Seppala <ese@mese.(none)>
2008-09-14 14:23:13 +0300

    puppy-dir: Added getopt and usage in anticipation for future command line switches

--- puppy-dir.c
+++ puppy-dir.c
@@ -4,41 +4,69 @@
 #include <stdlib.h>
 #include <time.h>
 #include <fnmatch.h>
+#include <getopt.h>
 
 #include "libpuppy.h"
 #include "tools.h"
 
+void usage(void)
+{
+    puts("usage: puppy-dir DataFiles/Foo '*.rec'");
+}
+
 int main(int a_c, char **a_v)
 {
-    puppy_t p = puppy_open(NULL);
-
-    if(puppy_ok(p))
+    int ok = 1;
+    
     {
-        char *dir = convert_slashes(a_c > 1 ? a_v[1] : "");
-        const char *glob = a_c > 2 ? a_v[2] : "*";
-
-        puppy_dir_entry_t *entries = puppy_hdd_dir(p, dir);
-        puppy_dir_entry_t *ptr = entries;
-
-        while(ptr->name[0])
-        {
-            if(fnmatch(glob, ptr->name, 0) == 0)
+        int opt;
+        
+        while (ok && (opt = getopt(a_c, a_v, "h")) != -1) {
+            switch(opt)
             {
-                char tmStr[1024];
-                struct tm tm = *localtime(&ptr->time);
-
-                strftime(tmStr, sizeof(tmStr), "%Y-%m-%d %H:%M:%S", &tm);
-                printf("%s %12lld  %s\n", tmStr, ptr->size, ptr->name);
+            case 'h':
+                usage();
+                ok = 0;
+                break;
+                
+            default:
+                ok = 0;
             }
-            ++ptr;
         }
-        free(entries);
-        free(dir);
     }
-    else
+
+    if(ok)
     {
-        fprintf(stderr, "Failed to initialize puppy\n");
+        puppy_t p = puppy_open(NULL);
+
+        if(puppy_ok(p))
+        {
+            char *dir = convert_slashes(a_c > optind ? a_v[optind] : "");
+            const char *glob = a_c > optind + 1 ? a_v[optind + 1] : "*";
+
+            puppy_dir_entry_t *entries = puppy_hdd_dir(p, dir);
+            puppy_dir_entry_t *ptr = entries;
+
+            while(ptr->name[0])
+            {
+                if(fnmatch(glob, ptr->name, 0) == 0)
+                {
+                    char tmStr[1024];
+                    struct tm tm = *localtime(&ptr->time);
+
+                    strftime(tmStr, sizeof(tmStr), "%Y-%m-%d %H:%M:%S", &tm);
+                    printf("%s %12lld  %s\n", tmStr, ptr->size, ptr->name);
+                }
+                ++ptr;
+            }
+            free(entries);
+            free(dir);
+        }
+        else
+        {
+            fprintf(stderr, "Failed to initialize puppy\n");
+        }
+        puppy_done(p);
     }
-    puppy_done(p);
     return 0;
 }
